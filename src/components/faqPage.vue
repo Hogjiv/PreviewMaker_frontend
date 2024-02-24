426741c32537120dc02696c310298ff4240c3d2e
Irina <hogjiv@gmail.com>
2024-02-24 22:48:29 +0700

    next

--- src/components/faqPage.vue
+++ src/components/faqPage.vue
@@ -17,80 +17,72 @@ export default {
 </script>
 
 <template>
-  <div v-if="!showFaqContent" class="d-flex flex-row" >
-    <div class="screen-block position-relative">
-    <div class="faqContent position-absolute">
-      <div
-        class="d-flex flex-row align-items-center justify-content-start mx-3 mt-3"
-      >
-        <div
-          class="d-flex btn btn-exit align-items-center align-self-center justify-content-center mx-2"
-          @click="toggleFaqContent"
-        >
-          <img
-            src="../../public/exit.svg"
-            class="icon-exit"
-            width="20"
-            height="20"
-          />
+  <div v-if="!showFaqContent" class="d-flex flex-row">
+    <div class="screen-block">
+      <div class="faqContent texts">
+        <div class="d-flex flex-row justify-content-between align-items-center mb-3">
+          <h3 class="ss ">How to use a script</h3>
+          <div class="btn btn-exit align-items-center justify-content-center" @click="toggleFaqContent">
+            <img src="../../public/exit.svg" class="icon-exit" width="20" height="20" style="fill: white;" />
+          </div>
         </div>
-        <h3 class="text-center">How to use a script</h3>
-      </div>
-      <div class="texts">
-        <p class="font-color-pink font-color-pink">
-          This script was created for myself for everyday usage. It's now
-          surpraised, that every archive, downloaded from any resource, could be
-          plased spmwhere in you PC and it's totally legal to use it every time
-          when you need a model. Problem is, not every archieve has a preview.
-          With previews you folders looks more organaize and simple to use.
-          Without previews it's not possible to understand what is inside, and
-          naming of every folder take too much time and not everytime clear to
-          understand what's inside. This script will organize you files and help
-          to work a little bit simpler and faster
-        </p>
-        <p>
-          1. Choose a directory where you models are <br />
-          2. Choose a directory where you want to put previews <br />
-          3. If you trying to make preview for first time it's highly
-          recommended to check up Hard scan function. It will scan all files in
-          choosen directory and save special data for take it again in any
-          moment. For example, you will have acsess to preview even when you
-          Internet connection doesn't work, as this file @remember@ page in
-          current state *
-          <br />
-          4. Choose Soft Scan if you alredy scaned files before. Choose this
-          option, if you added new models to folder or did some other changes
-          (deleted files, added new). This option will rewrite you special file
-          and you will have acsess to your refreshed previews <br />
-          5. After Soft Scan or Hard scan hase been choosed, push Run Script
-          button! Try to push "Show progress" now <br />
-          6. Now you can see a table with all models, wich was scanned. You can
-          see models name as it's called in archive name and name of models. It
-          can take time to scan your files and find in on website. You can see
-          progress in progress bar, located upper of the table ** <br />
-          // 7. If you already scaned a directory and have magic Json file in
-          you directory, you can search anything with keyword in Search bar.
-          Just write name of model, production or other and you can find if you
-          have nesessary model. Very fast and useful!<br />
-          * It's now important if you have zip/rar or unarchived files. My
-          script will do everything with any state of you files. <br />
-          ** Sometimes it can take a lot of time to scan files and find all
-          previews on website. Sometimes 3dd doesn't respond correctly and you
-          need to reload page and start again, unfortunately.
-        </p>
-      </div>
+
+
+
+
+        <div class=" ">
+          <p class="font-color-pink font-color-pink align-items-center text-center text-uppercase mb-3">
+            Take a preview of any model and make your work simpler and faster!
+          </p>
+          <p>
+            1. Choose a directory where you models are <br />
+            2. Choose a directory where you want to put previews <br />
+            3. If you trying to make preview for first time it's highly
+              recommended to check up  <b class="font-color-pink">HARD SCAN  </b> function. It will scan all files in
+            choosen directory and save special data for take it again in any
+            moment. For example, you will have acsess to preview even when you
+            Internet connection doesn't work *
+            <br />
+            4. Choose  <b class="font-color-pink">SOFT SCAN  </b>if you alredy scaned files before. Choose this
+            option,  <b class="font-color-pink"> if you added new models to folder or did some other changes </b>
+            (deleted files, added new). This option will rewrite you special file
+            and you will have acsess to your refreshed previews <br />
+            5. After Soft Scan or Hard scan hase been choosed, push <b class="font-color-pink">  Run Script  button!  </b>
+           Try to push "Show progress" now <br />
+            6. Now you can see a table with all models, wich was scanned. It
+            can take a few time to scan your files and find in on website  **. You can see
+            progress in progress bar, located upper of the table <br />
+            * It's not important if you have zip/rar or unarchived files. My
+            script will do everything with any state of you files. <br />
+            ** Sometimes it can take a lot of time to scan files and find all
+            previews on website. Sometimes 3dd doesn't respond correctly and you
+            need to reload page and start again, unfortunately.
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+.ss {
 
+  margin-left: 80px;
+}
+
+.pp {
+  max-width: 450px;
+  height: 100%;
+}
+
+.ii {
+  background-color: aquamarine;
+}
 
 .btn-exit {
   width: 40px;
   height: 40px;
-  background-color: #c97191;
+  background-color: #474747;
 }
 
 .btn-exit:hover {
@@ -101,34 +93,36 @@ export default {
   background-color: pink !important;
 }
 
-.screen-block { 
-  width: 100vw;
-  height: 100vh;
-z-index: 1;
+.screen-block {
+  right: 0px;
+  max-width: 450px;
 }
 
 .faqContent {
-  width: 500px;
-  min-width: 300px;
-  min-height: 500px;
+
+  right: 0px;
   height: 100vh;
-  background-color: #0c0c0ce0;
+  background-color: #474747;
   color: white;
   overflow-y: auto;
   animation: anim 1s ease-in-out;
-  right: 0;
-  z-index: 2
+
 }
+
 @keyframes anim {
   from {
-    transform: translateY(-100%); /* Сдвигаем блок за пределы экрана справа */
+    transform: translateY(-100%);
+    /* Сдвигаем блок за пределы экрана справа */
   }
+
   to {
-    transform: translateY(0); /* Сдвигаем блок обратно, чтобы он оказался видимым */
+    transform: translateY(0);
+    /* Сдвигаем блок обратно, чтобы он оказался видимым */
   }
 }
+
 .texts {
-  padding: 10px 30px 10px 30px; /* Добавьте отступы сюда в формате: верх право низ лево */
+  padding: 10px 30px 10px 30px;
+  /* Добавьте отступы сюда в формате: верх право низ лево */
 }
- 
 </style>
--- src/views/scriptUi.vue
+++ src/views/scriptUi.vue
@@ -80,9 +80,9 @@ export default {
     },
     isButtonDisabled() {
       const modelPathValid =
-          this.modelPath.trim() && /^[A-Z]\W+.*$/gm.test(this.modelPath);
+        this.modelPath.trim() && /^[A-Z]\W+.*$/gm.test(this.modelPath);
       const imagePathValid =
-          this.imagePath.trim() && /^[A-Z]\W+.*$/gm.test(this.imagePath);
+        this.imagePath.trim() && /^[A-Z]\W+.*$/gm.test(this.imagePath);
       return !modelPathValid || !imagePathValid;
     },
     makePreview() {
@@ -93,10 +93,10 @@ export default {
         hardScan: this.hardScan,
       });
       console.log(
-          "modelpath = ", this.modelPath,
-          "imgPath = ", this.imagePath,
-          "softscan = ", this.softScan,
-          "hardscan = ", this.hardScan)
+        "modelpath = ", this.modelPath,
+        "imgPath = ", this.imagePath,
+        "softscan = ", this.softScan,
+        "hardscan = ", this.hardScan)
     },
   },
 };
@@ -106,103 +106,66 @@ export default {
   <div class="header d-flex justify-content-center align-items-center">
     <h2 class="bold-text font-color-dark text-uppercase">Preview maker</h2>
   </div>
- 
-  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+  <div v-if="faqOpen">
+     <div class="modal d-flex"
+      id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <faqPage @btnClosed='btnClosed'>
+        sdf
+      </faqPage>
     </div>
   </div>
-</div>
-
-  <div
-      v-if="faqOpen"
-      class="faqBlock d-flex col-lg align-items-center align-self-center justify-content-center position-fixed-lg">
-    <faqPage
-        @btnClosed='btnClosed'
-    >
-    </faqPage>
-  </div> 
+ 
 
   <div class="fluid-container d-flex flex-row">
     <div class="container-lg d-flex flex-column justify-content-center align-items-center">
       <div class="container-lg d-flex flex-row justify-content-center align-items-center menu">
-        <div class="row col-12 d-flex justify-content-center "> 
-             <div class="d-flex flex-column col-lg-2 col-md-12 col-sm-12 col-12 justify-content-center align-items-center mt-3 mb-3 order-lg-1 order-sm-2 order-2  ">  
+        <div class="row col-12 d-flex justify-content-center ">
+          <div
+            class="d-flex flex-column col-lg-2 col-md-12 col-sm-12 col-12 justify-content-center align-items-center mt-3 mb-3 order-lg-1 order-sm-2 order-2  ">
             <div class="d-flex mb-2  ">
-              <checkBox v-model="softScan"/>
+              <checkBox v-model="softScan" />
               <p class="font-size-16 mx-2  ">
                 Soft scan
               </p>
             </div>
             <div class="d-flex  ">
               <!-- <div class="d-flex align-self-lg-start align-self-sm-center align-self-center"> -->
-              <checkBox v-model="hardScan"/>
+              <checkBox v-model="hardScan" />
               <p class="font-size-16 mx-1    ">
                 Hard scan
               </p>
             </div>
           </div>
           <div
-              class="d-flex flex-column justify-content-center align-items-center col-lg-8 col-md-12 col-sm-12 col-12 order-lg-2 order-sm-1 order-1">
-            <input
-                type="text"
-                v-model="modelPath"
-                class="input-form"
-                @input="saveDataModel"
-                placeholder="Path for load models"
-            />
-            <input
-                type="text"
-                v-model="imagePath"
-                class="input-form mt-3"
-                @input="saveDataImage"
-                placeholder="Path for save image"
-            />
+            class="d-flex flex-column justify-content-center align-items-center col-lg-8 col-md-12 col-sm-12 col-12 order-lg-2 order-sm-1 order-1">
+            <input type="text" v-model="modelPath" class="input-form" @input="saveDataModel"
+              placeholder="Path for load models" />
+            <input type="text" v-model="imagePath" class="input-form mt-3" @input="saveDataImage"
+              placeholder="Path for save image" />
           </div>
 
 
           <div
-              class="d-flex faq flex-column col-md col-lg-2 col-sm-6 align-items-center align-self-center justify-content-center  order-lg-3 order-sm-3 order-3">
-            <button class="btn btn-faq bold-text font-size-32"
-                    @click="faqOpen = !faqOpen"
-            >
+            class="d-flex faq flex-column col-md col-lg-2 col-sm-6 align-items-center align-self-center justify-content-center  order-lg-3 order-sm-3 order-3">
+            <button class="btn btn-faq bold-text font-size-32" @click="faqOpen = !faqOpen">
               FAQ
             </button>
           </div>
         </div>
       </div>
-      <div
-          class="containter d-flex flex-column justify-content-center align-items-center">
+      <div class="containter d-flex flex-column justify-content-center align-items-center">
         <div class="btn-block d-flex flex-column">
           <div class="d-flex justify-content-center align-items-center">
-            <button
-                class="big-btn mt-3 col-12 align-items-center"
-                :class="{
-                btnActive: btnActive && $store.state.scriptRunning,
-                btnDisabled: !$store.state.scriptRunning,
-              }"
-                @click="btnClicked">
+            <button class="big-btn mt-3 col-12 align-items-center" :class="{
+              btnActive: btnActive && $store.state.scriptRunning,
+              btnDisabled: !$store.state.scriptRunning,
+            }" @click="btnClicked">
               <p class="bold-text font-size-22 text-white text-center btnDisables">
                 {{
                   btnActive && $store.state.scriptRunning
-                      ? "Running!"
-                      : "Make previews!"
+                  ? "Running!"
+                  : "Make previews!"
                 }}
               </p>
               <div class="popUp" v-if="showPopUp">
@@ -211,19 +174,14 @@ export default {
             </button>
           </div>
           <div v-if="btnActive">
-            <div
-                class="d-flex col-12 justify-content-center align-items-center">
-              <button
-                  class="big-btn mt-3"
-                  @click="showProgress = !showProgress">
+            <div class="d-flex col-12 justify-content-center align-items-center">
+              <button class="big-btn mt-3" @click="showProgress = !showProgress">
                 <p class="bold-text font-size-22 font-color-dark text-center">
-                  <span v-if="showProgress"
-                        class="text-white">
-                      Show Progress
+                  <span v-if="showProgress" class="text-white">
+                    Show Progress
                   </span>
-                  <span v-else
-                        class="text-white">
-                      Show less
+                  <span v-else class="text-white">
+                    Show less
                   </span>
                 </p>
               </button>
@@ -231,33 +189,22 @@ export default {
 
             <div v-if="!showProgress" class="preview-window mt-5">
               <div v-if="$store.state.scriptRunning">
-                <progressBar
-                    class="my-4 d-flex justify-content-center align-items-center"
-                >
+                <progressBar class="my-4 d-flex justify-content-center align-items-center">
                 </progressBar>
               </div>
-              <h2
-                  v-if="!$store.state.scriptRunning"
-                  class="justify-content-center align-items-center text-center my-4 bold-text font-size-24 font-color-pink text-uppercase"
-              >
+              <h2 v-if="!$store.state.scriptRunning"
+                class="justify-content-center align-items-center text-center my-4 bold-text font-size-24 font-color-pink text-uppercase">
                 Finish!
               </h2>
               <div class="container w-100">
                 <div class="row justify-content-center align-items-center">
-                  <div
-                      class="card col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-center mt-4 mx-3"
-                      v-for="model in $store.state.modelsList"
-                      :key="model.name">
-                    <img
-                        class="model-image my-3"
-                        v-if="model.image"
-                        :src="model.image"/>
-                    <p
-                        class="font-size-14 font-color-dark medium-text text-center">
+                  <div class="card col-12 col-sm-6 col-md-4 col-lg-3 d-flex align-items-center mt-4 mx-3"
+                    v-for="model in $store.state.modelsList" :key="model.name">
+                    <img class="model-image my-3" v-if="model.image" :src="model.image" />
+                    <p class="font-size-14 font-color-dark medium-text text-center">
                       {{ model.name }}
                     </p>
-                    <p
-                        class="font-size-14 font-color-pink text-center medium-text">
+                    <p class="font-size-14 font-color-pink text-center medium-text">
                       {{ model.title }}
                     </p>
                   </div>
@@ -271,24 +218,30 @@ export default {
   </div>
 </template>
 <style>
-.gg {
-  background-color: orange;
-}
-.faqBlock {
-  top: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1;
+.modal {
   position: absolute;
-   /* position: fixed;   */
- width: 300px;  
+  display: flex;
+  right: 0;
+  left: auto;
+  max-width: 450px;   
 }
- 
+
+.hh {
+  background-color: tomato;
+  height: 300px;
+  widows: 300px;
+  margin: 0px;
+  top: 0;
+  left: 0px;
+  bottom: 0;
+}
+
 @keyframes slideIn {
   from {
     transform: translateX(-20%);
     opacity: 0;
   }
+
   to {
     transform: translateX(0);
     opacity: 1;
@@ -302,7 +255,8 @@ export default {
 }
 
 
-.btn-faq:hover, btn-faq:active {
+.btn-faq:hover,
+btn-faq:active {
   background-color: #c97191;
 }
 
@@ -380,14 +334,12 @@ p {
 .btnActive {
   background-color: #c97191;
   background-size: 200% 100%;
-  background-image: linear-gradient(
-      to right,
+  background-image: linear-gradient(to right,
       #de93af 0%,
       #a97ec2 25%,
       #de93af 50%,
       #a97ec2 75%,
-      #de93af 100%
-  );
+      #de93af 100%);
   transition: background-position 1s linear;
   animation: gradientAnimation 6s linear infinite;
 }
@@ -415,12 +367,14 @@ p {
   0% {
     background-position: 100% 0%;
   }
+
   100% {
     background-position: -100% 0%;
   }
 }
 
 @keyframes error-blink {
+
   0%,
   20%,
   40%,
@@ -477,5 +431,4 @@ p {
     opacity: 0;
     display: none;
   }
-}
-</style>
+}</style>
